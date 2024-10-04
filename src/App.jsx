a0d9c194170479ef4ed7a780b31c8b13e769fe60
Multiprocesssing-CODER <sagnik.dey745@icloud.com>
2024-10-04 22:29:37 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+import Sidebar from './components/Sidebar/Sidebar'
+
+const App = () => {
+  return (
+    <>
+      <Sidebar />
+    </>
+  )
+}
+
+export default App
